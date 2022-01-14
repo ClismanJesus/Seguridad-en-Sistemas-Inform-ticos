ffd647e06b83ef86552a478f8fbade4de7e5ac57
ClismanJesus <cmorenom@uni.pe>
2022-01-14 00:48:10 -0500

    probando las configuraciones de selenium con el webdriver de firefox

---
+++ Practica 1/Pregunta1.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox(executable_path=r'/home/cmorenom/Escritorio/Portafolio GitHub/Ciberseguridad/Practica 1/geckodriver')
+
+driver.get('https://github.com')
